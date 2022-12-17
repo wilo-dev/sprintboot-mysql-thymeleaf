fa10013854156a874c37483b862762094622f48a
@ewilliaml <willian_enrriquez89@hotmail.com>
2022-12-17 11:36:07 -0500

    creacion de vista de errores

--- src/main/java/com/wilo/springbootjpamysql/app/MvcConfig.java
+++ src/main/java/com/wilo/springbootjpamysql/app/MvcConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Paths;
@@ -12,6 +13,7 @@ import java.nio.file.Paths;
 public class MvcConfig implements WebMvcConfigurer {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+    // esta clase sirve para subir img en un directorio estatico upload
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        WebMvcConfigurer.super.addResourceHandlers(registry);
@@ -28,4 +30,9 @@ public class MvcConfig implements WebMvcConfigurer {
 //                .addResourceLocations(resourcePath);
 //
 //    }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/error_403").setViewName("error/error_403");
+    }
+
 }
