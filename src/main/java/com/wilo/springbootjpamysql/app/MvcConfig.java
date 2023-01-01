9a910c2bfba7ab9abfe7d97afabda89c626dba1d
@ewilliaml <willian_enrriquez89@hotmail.com>
2022-12-31 19:22:27 -0500

    internacionalizacion

--- src/main/java/com/wilo/springbootjpamysql/app/MvcConfig.java
+++ src/main/java/com/wilo/springbootjpamysql/app/MvcConfig.java
@@ -5,11 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.nio.file.Paths;
+import java.util.Locale;
+
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -42,4 +46,24 @@ public class MvcConfig implements WebMvcConfigurer {
         return new BCryptPasswordEncoder();
     }
 
+    // multilenguaje
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("es", "ES"));
+        return localeResolver;
+    }
+
+    // interceptor para cambiar el idioma
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
